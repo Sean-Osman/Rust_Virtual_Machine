8482631c13f0d706b4965d288b0e4d3c2df79c48
Sean Osman <96084662+Sean-Osman@users.noreply.github.com>
2025-09-23 12:28:57 -0600

    change main project file

--- Virtual_Machine_Project/target/debug/deps/Virtual_Machine_Project-23b5ee6ae1538e57.d
+++
@@ -1,5 +0,0 @@
-C:\Users\spoth\Documents\GitHub\Rust_Virtual_Machine\Virtual_Machine_Project\target\debug\deps\Virtual_Machine_Project-23b5ee6ae1538e57.d: src\main.rs
-
-C:\Users\spoth\Documents\GitHub\Rust_Virtual_Machine\Virtual_Machine_Project\target\debug\deps\libVirtual_Machine_Project-23b5ee6ae1538e57.rmeta: src\main.rs
-
-src\main.rs:
