bb4dd647a9c47fd527489a397309d54811a5cbd7
nansan4 <nando@santosalliances.com>
2025-09-24 12:59:33 -0600

    need to finish "add constant"

--- bytecode_vm/src/lib.rs
+++ bytecode_vm/src/lib.rs
@@ -17,9 +17,9 @@ pub enum OpCode{
 #[derive(Debug)]
 struct Chunk{
 
-    code: Vec<u8>,
-    lines: Vec<u32>,
-    values: Vec<Value>
+    code: Vec<u8>, //what opcode
+    lines: Vec<u32>, //specific line location
+    values: Vec<Value> // the actual number
 
 }
 
@@ -30,9 +30,7 @@ impl Chunk{
 
     }
 
-    fn add_constant(&self, num:u32){
-
-    }
+    
 
     fn write_to_chunk(&mut self, byte:u8, linenum:u32){
         self.code.push(byte);
@@ -56,7 +54,35 @@ impl Chunk{
  impl OpCode{
 
 
-    fn OpConstant(){
-        
+    fn OpToBit(name: OpCode) -> u8{
+
+        match(name){
+            OpCode::OpReturn => 0,
+            OpCode::OpConstant => 1,
+            OpCode::OpNegate => 2,
+            OpCode::OpAdd => 3,
+            OpCode::OpSubtract => 4,
+            OpCode::OpMultiply => 5,
+            OpCode::OpDivide => 6
+        }
+    }
+
+    fn BitToOp(num: u8) -> OpCode{
+
+        match(num){
+            0 => OpCode::OpReturn,
+            1 => OpCode::OpConstant,
+            2 => OpCode::OpNegate,
+            3 => OpCode::OpAdd,
+            4 => OpCode::OpSubtract,
+            5 => OpCode::OpMultiply,
+            6 => OpCode::OpDivide,
+            _ => unreachable!()
+        }
+
+    }
+
+    fn add_constant(num: u8){
+
     }
  }
