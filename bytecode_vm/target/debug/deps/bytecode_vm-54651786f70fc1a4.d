773ad798443cede717fae493a2b7bed2c12098bc
nansan4 <nando@santosalliances.com>
2025-10-17 20:45:06 -0600

    bill merge

--- bytecode_vm/target/debug/deps/bytecode_vm-54651786f70fc1a4.d
+++ bytecode_vm/target/debug/deps/bytecode_vm-54651786f70fc1a4.d
@@ -1,7 +1,8 @@
-C:\Users\nando\documents\VirtualMachine\bytecode_vm\target\debug\deps\bytecode_vm-54651786f70fc1a4.d: src\lib.rs
+C:\Users\nando\documents\VirtualMachine\bytecode_vm\target\debug\deps\bytecode_vm-54651786f70fc1a4.d: src\lib.rs src\scanner.rs
 
-C:\Users\nando\documents\VirtualMachine\bytecode_vm\target\debug\deps\libbytecode_vm-54651786f70fc1a4.rlib: src\lib.rs
+C:\Users\nando\documents\VirtualMachine\bytecode_vm\target\debug\deps\libbytecode_vm-54651786f70fc1a4.rlib: src\lib.rs src\scanner.rs
 
-C:\Users\nando\documents\VirtualMachine\bytecode_vm\target\debug\deps\libbytecode_vm-54651786f70fc1a4.rmeta: src\lib.rs
+C:\Users\nando\documents\VirtualMachine\bytecode_vm\target\debug\deps\libbytecode_vm-54651786f70fc1a4.rmeta: src\lib.rs src\scanner.rs
 
 src\lib.rs:
+src\scanner.rs:
--- bytecode_vm/target/debug/libbytecode_vm.d
+++ bytecode_vm/target/debug/libbytecode_vm.d
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-C:\Users\nando\documents\VirtualMachine\bytecode_vm\target\debug\libbytecode_vm.rlib: C:\Users\nando\documents\VirtualMachine\bytecode_vm\src\lib.rs
-=======
-c:\Users\spoth\Documents\GitHub\Rust_Virtual_Machine\bytecode_vm\target\debug\libbytecode_vm.rlib: c:\Users\spoth\Documents\GitHub\Rust_Virtual_Machine\bytecode_vm\src\lib.rs
->>>>>>> sean-branch
+C:\Users\nando\documents\VirtualMachine\bytecode_vm\target\debug\libbytecode_vm.rlib: C:\Users\nando\documents\VirtualMachine\bytecode_vm\src\lib.rs C:\Users\nando\documents\VirtualMachine\bytecode_vm\src\scanner.rs
